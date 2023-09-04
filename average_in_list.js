f57cad70678631d9cd90615df7d15e80b8fc7bf3
Andra Helton <andrahelton@gmail.com>
2023-09-03 22:31:55 -0600

    Add pseudocode

---
+++ average_in_list.js
@@ -0,0 +1,10 @@
+// input: array of integers
+// output: one integer
+
+// PSEUDOCODE
+// variable count set to zero
+// loop through input array
+// for every number in the array, add that to the variable count 
+// after getting the sum of all numbers and saving to variable count,
+// divide count by the input array size
+
