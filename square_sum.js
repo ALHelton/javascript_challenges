6111f413dbb4daa5249d27eab91c2494e0cc3300
Andra Helton <andrahelton@gmail.com>
2023-09-03 21:50:47 -0600

    Add pseudocode

---
+++ square_sum.js
@@ -0,0 +1,12 @@
+// input: array of integers
+// output: one integer
+
+// PSEUDOCODE
+// declare variable, sum, that is set to equal 0
+// must loop through the array
+// For every integer in the array,
+// must times the integer by itself
+// then must add the result to the sum variable
+// must return the sum variable (integer)
+// after looping operations
+
